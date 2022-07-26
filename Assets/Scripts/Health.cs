09c36d8de3436e6ec8e6749798c48a05055fb0fa
Ernest Subotć <gamedevsubo@gmail.com>
2022-07-26 07:16:28 +0200

    Add scoring and UI

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,11 +7,15 @@ public class Health : MonoBehaviour
     protected int health;
     [SerializeField] private int healthMax;
     protected ObjectPooler objectPool;
+    private Scoring score;
 
     void Start()
     {
         var temp = GameObject.Find("Object Pool");
         objectPool = temp.GetComponent<ObjectPooler>();
+
+        var GC = GameObject.Find("Game Controller");
+        score = GC.GetComponent<Scoring>();
     }
     void OnEnable()
     {
@@ -29,6 +33,13 @@ public class Health : MonoBehaviour
 
     protected virtual void Death()
     {
+        //pull an explosion from the pooler
+        //pull a drop from the pooler
+
+        //add points
+        score.AddPoints(healthMax);
+        score.IncrementMultiplier();
+
         gameObject.SetActive(false);
     }
 }
