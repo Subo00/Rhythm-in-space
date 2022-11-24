c7293fe49be126535363e0eb6999eb4025ba7b34
subo <gamedevsubo@gmail.com>
2022-11-24 09:47:12 +0100

    Clamp player's movement

--- Assets/Scripts/Space Ships/FollowMouse.cs
+++ Assets/Scripts/Space Ships/FollowMouse.cs
@@ -4,6 +4,8 @@ public class FollowMouse : MonoBehaviour
 {
     Vector3 mousePosition;
     [SerializeField] private float moveSpeed = 0.3f;
+    [SerializeField] private float minX;
+    [SerializeField] private float minY;
     Rigidbody2D rb;
     Vector2 position = new Vector2(0f, 0f);
     Camera mainCamera;
@@ -18,6 +20,8 @@ public class FollowMouse : MonoBehaviour
     {
         mousePosition = Input.mousePosition; //gets position in pixels
         mousePosition = mainCamera.ScreenToWorldPoint(mousePosition); //gets position in world points
+        mousePosition.x = Mathf.Clamp(mousePosition.x, minX, -minX); //the difference between max and min       
+        mousePosition.y = Mathf.Clamp(mousePosition.y, minY, -minY); //values is the minus        
         position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
     }
 
